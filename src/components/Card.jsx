c8b2f30f96d0eb1b1221df5b3beb78aac2953014
juarezmeneses <juarezlmfilho@gmail.com>
2022-06-17 18:40:21 -0300

    Project create

---
+++ src/components/Card.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Card() {
+  return (
+    <div className="bg-white p-6 mt-10 rounded-lg shadow-md">
+      <div className="text-center">
+        <span className="block text-lg font-bold text-slate-700">Fortaleza</span>
+        <span className="text-slate-400 text-sm font-medium">Ceará, Brasil</span>
+      </div>
+
+      <div className="font-bold text-slate-700 flex mt-4 mb-2">
+        <span className="text-8xl">27</span>
+        <span className="text-2xl mt-2">ºC</span>
+      </div>
+
+      <div className="text-center">
+        <span className="block">icone</span>
+        <span className="text-slate-700 font-medium">nublado</span>
+      </div>
+    </div>
+
+  );
+}
+
+export default Card;
